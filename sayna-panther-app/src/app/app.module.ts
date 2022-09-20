80576e7b1b198eb150c8945615e32152a30af2c8
Tony <nya.tony2010@gmail.com>
2022-09-20 15:03:14 +0100

    create structural components

--- sayna-panther-app/src/app/app.module.ts
+++ sayna-panther-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ScrollbarComponent } from './components/scrollbar/scrollbar.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ScrollbarComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
