b375217ab5b380b41d67be6c51067901ce3dc443
Tony <nya.tony2010@gmail.com>
2022-09-24 20:07:08 +0100

    add comments & pagination

--- sayna-panther-app/src/app/components/single-product/single-product.component.ts
+++ sayna-panther-app/src/app/components/single-product/single-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiArticlesService } from 'src/app/services/api-articles.service';
+import { ApiCommentsService } from 'src/app/services/api-comments.service';
 
 @Component({
   selector: 'app-single-product',
@@ -9,14 +10,20 @@ import { ApiArticlesService } from 'src/app/services/api-articles.service';
 })
 export class SingleProductComponent implements OnInit {
   product!: any;
+  page: number = 1;
+  public comments: any;
 
-  constructor(private api: ApiArticlesService,private route: ActivatedRoute) { }
+  constructor(private api: ApiArticlesService,private route: ActivatedRoute, private apiCom: ApiCommentsService) { }
 
   ngOnInit(): void {
     const productID = +this.route.snapshot.params['id'];
 
     this.api.getProductID(productID).subscribe((res) => { 
       this.product = res;
+    });
+
+    this.apiCom.getComments().subscribe((result) => {
+      this.comments = result;
     });
   }
 
--- sayna-panther-app/src/app/services/api-comments.service.ts
+++ sayna-panther-app/src/app/services/api-comments.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiCommentsService {
+  url: string = 'http://localhost:3000/comments/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getComments() {
+    return this.http.get<any>(this.url).pipe(map((res) => {
+      return res;
+    }));
+  }
 }
