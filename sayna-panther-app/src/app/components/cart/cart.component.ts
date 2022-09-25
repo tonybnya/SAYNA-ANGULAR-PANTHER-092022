81d9360ae58e3a55b1d152aa19f018050e231fe6
Tony <nya.tony2010@gmail.com>
2022-09-25 09:53:36 +0100

    add routing to the cart

--- sayna-panther-app/src/app/components/cart/cart.component.ts
+++ sayna-panther-app/src/app/components/cart/cart.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  public products: any = [];
+  public grandTotal!: number;
+  constructor(private cartService: CartService, private router: Router) {}
 
   ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res) => {
+      this.products = res;
+      this.grandTotal = +this.cartService.getTotalPrice();
+    });
   }
 
+  removeItem(product: any) {
+    if (confirm('Es-tu sûr de vouloir supprimer ce produit'))
+      this.cartService.removeCartItem(product);
+  };
+  // removeItem(productId: any) {
+  //   if (confirm('Es-tu sûr de vouloir supprimer ce produit'))
+  //     this.cartService.removeCartItem(productId).subscribe(() => {
+  //       alert('Suppression confirmée !');
+  //       window.location.reload();
+  //     });
+  // }
 }
--- sayna-panther-app/src/app/services/cart.service.ts
+++ sayna-panther-app/src/app/services/cart.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  public cartItemList: any = [];
+  public productList = new BehaviorSubject<any>([]);
 
   constructor() { }
+
+  getProducts() {
+    return this.productList.asObservable();
+  }
+
+  addToCart(product: any) {
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+  }
+
+  getTotalPrice() {
+    let cartTotal = 0;
+    this.cartItemList.map((a: any) => {
+      cartTotal += +a.total;
+    });
+    return cartTotal;
+  }
+
+  removeCartItem(product: any) {
+    this.cartItemList.map((a: any, index: any) => {
+      this.cartItemList.splice(index, 1);
+    });
+    this.productList.next(this.cartItemList);
+  }
 }
